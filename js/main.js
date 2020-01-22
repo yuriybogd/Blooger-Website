f45de4fe18676d82d92ce5b5ba47d5488289fa59
Yury <yuriy.bogd@yandex.ru>
2020-01-23 02:09:55 +0300

    added responsive and aos -animation of scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const responsive = {
+    0 : {
+        items: 1
+    },
+    320 : {
+        items: 1
+    },
+    560 : {
+        items: 2
+    },
+    960 : {
+        items: 3
+    }
+}
+
 $(document).ready(function(){
 
     $nav = $('.nav');
@@ -8,7 +23,6 @@ $(document).ready(function(){
         $nav.toggleClass('collapse');
     })
 
-
     //owl carousel for blog
     $('.owl-carousel').owlCarousel({
         loop: true,
@@ -16,10 +30,20 @@ $(document).ready(function(){
         autoplayTimeout: 3000,
         dots: false,
         nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+
+    });
+
+    //click to scroll top
+    $('.move-up').click(function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000)
     });
 
 
-
+    //AOS Instance
+    AOS.init();
 });
 
